f69759b4d46766f12ed8d93933aa2f47d7622f70
unknown <2020a1r141@mietjammu.in>
2021-03-21 23:33:56 +0530

    files are added

---
+++ 19_Length_of_String.c
@@ -0,0 +1,16 @@
+//Author Name:Harsh Mahajan
+//WAP on length of string 
+#include<string.h>
+main()
+{
+	char str1[20]="hello";
+	int length;
+
+	printf("length of string str1=");
+
+	length=strlen(str1);
+
+	printf("%d",length);
+
+	return 0;
+} //end of the main body function 
---
+++ 24_free().c
@@ -0,0 +1,12 @@
+//Author Name:Harsh Mahajan
+//WAP to demonstrate the use of free()
+#include <stdio.h> //Using standard input output header file.
+#include <stdlib.h> //Using standard library header file.
+    int main() {
+    int* ptr = malloc(10 * sizeof(*ptr));
+        if (ptr != NULL){
+        *(ptr + 2) = 50;
+        printf("Value of the 2nd integer is %d",*(ptr + 2));
+        }
+    free(ptr);
+} //End of the main function body
