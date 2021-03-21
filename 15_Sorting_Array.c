6284c2902ff886e8215270e8db11eca8a1f381f2
unknown <2020a1r141@mietjammu.in>
2021-03-21 15:41:01 +0530

    printing array

---
+++ 15_Sorting_Array.c
@@ -0,0 +1,36 @@
+//Author name:Harsh Mahajan
+//WAP on sorting an array using Insertion Sort
+#include <stdio.h>
+int main()
+{
+    int n, i, j, temp;
+    int arr[64];
+   
+    printf("Enter number of elements\n");
+    scanf("%d", &n);
+
+    printf("Enter %d integers\n", n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for (i = 1 ; i <= n - 1; i++) //Using for loop.
+    {
+	    j = i;
+            while ( j > 0 && arr[j-1] > arr[j])
+            {
+            //Swap function will help for the purpose.       
+                temp     = arr[j];
+                arr[j]   = arr[j-1];
+                arr[j-1] = temp;
+                j--;
+            }
+    }
+    //This will give the output.
+    printf("Sorted list in ascending order:\n");
+    for (i = 0; i <= n - 1; i++)
+    {
+        printf("%d\n", arr[i]);
+    }
+    return 0;
+} //End of the main function body
