5b457e0a80104b464e495c6064de41be3e915e5b
unknown <2020a1r141@mietjammu.in>
2021-04-01 13:44:20 +0530

    uploading programs

---
+++ do_while.c
@@ -0,0 +1,13 @@
+//Author name:Harsh Mahajan
+//WAP on do while  
+#include <stdio.h>
+int main()
+{
+	int j=0;
+	do
+	{
+		printf("Value of variable j is: %d\n", j);
+		j++;
+	}while (j<=3);
+	return 0;
+}
